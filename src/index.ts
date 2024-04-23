9800de288ac0ebb488cc67943b67d1ba2eca57f5
liuqi <liuqi6602@163.com>
2024-04-23 14:32:48 +0800

    124124

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,36 @@
 import { env } from 'process'
-import { readFileSync } from 'fs'
-import { Octokit } from 'octokit'
-import { failedWithLogs } from './logs.js'
+// import { readFileSync } from 'fs'
+// import { Octokit } from 'octokit'
+// import { failedWithLogs } from './logs.js'
 
-const catchEnv = (name: string) => {
-  const value = env[name]
-  if (value) return Promise.resolve(value)
-  else return Promise.reject(`未找到名为${name}的环境变量`)
-}
+console.log('GITHUB_TOKEN: ', env['GITHUB_TOKEN'])
+console.log('TOKEN_FOR_ACTIONS: ', env['TOKEN_FOR_ACTIONS'])
+console.log('NPM_TOKEN: ', env['NPM_TOKEN'])
 
-const versionNumber = JSON.parse(
-  readFileSync('./package.json', { encoding: 'utf-8' })
-).version as string
+// const catchEnv = (name: string) => {
+//   const value = env[name]
+//   if (value) return Promise.resolve(value)
+//   else return Promise.reject(`未找到名为${name}的环境变量`)
+// }
 
-const vVersion = 'v'.concat(versionNumber)
+// const versionNumber = JSON.parse(
+//   readFileSync('./package.json', { encoding: 'utf-8' })
+// ).version as string
 
-catchEnv('RELEASE_TOKEN')
-  .then(async (auth) => {
-    console.log(env)
-    const userAgent = `@polarove/releaseBetweenTags/${vVersion}`
-    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
-    const octokit = new Octokit({
-      auth,
-      userAgent,
-      timeZone
-    })
-    const { data: user } = await octokit.rest.users.getAuthenticated()
-    console.log('username:', user.login)
-  })
-  .catch((err) => failedWithLogs(err))
+// const vVersion = 'v'.concat(versionNumber)
+
+// catchEnv('RELEASE_TOKEN')
+//   .then(async (auth) => {
+//     console.log('GITHUB_TOKEN', env['GITHUB_TOKEN'])
+//     console.log('RELEASE_TOKEN', auth)
+//     const userAgent = `@polarove/releaseBetweenTags/${vVersion}`
+//     const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
+//     const octokit = new Octokit({
+//       auth,
+//       userAgent,
+//       timeZone
+//     })
+//     const { data: user } = await octokit.rest.users.getAuthenticated()
+//     console.log('username:', user.login)
+//   })
+//   .catch((err) => failedWithLogs(err))

e1b07722a69e77f7ec2ec574f6e6a0b3cfcb7460
liuqi <liuqi6602@163.com>
2024-04-23 14:48:51 +0800

    346

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { env } from 'process'
 import { readFileSync } from 'fs'
 import { Octokit } from 'octokit'
 import { failedWithLogs } from './logs.js'
+import { getStringAfter } from './utils.js'
 
 // const GITHUB_TOKEN = env['GITHUB_TOKEN']
 // const RELEASE_TOKEN = env['RELEASE_TOKEN']
@@ -40,5 +41,32 @@ catchEnv('RELEASE_TOKEN')
     })
     const { data: user } = await octokit.rest.users.getAuthenticated()
     console.log('username:', user.login)
+    generate()
   })
   .catch((err) => failedWithLogs(err))
+
+const generate = async () => {
+  const releaseToken = await catchEnv('RELEASE_TOKEN')
+  const ref = await catchEnv('GITHUB_REF')
+  const repository = await catchEnv('GITHUB_REPOSITORY')
+  console.log(
+    releaseToken,
+    getStringAfter(ref, '/', 2),
+    getStringAfter(repository, '/')
+  )
+}
+
+// await octokit.request('POST /repos/{owner}/{repo}/releases', {
+//   owner: user.login,
+//   repo: data.repositoryName,
+//   tag_name: vVersion,
+//   target_commitish: data.targetCommitish,
+//   name: vVersion,
+//   body: 'Description of the release',
+//   draft: false,
+//   prerelease: false,
+//   generate_release_notes: false,
+//   headers: {
+//     'X-GitHub-Api-Version': '2022-11-28'
+//   }
+// })

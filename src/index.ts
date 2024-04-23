28b1500f0188dedff926bc965a9159073f421271
liuqi <liuqi6602@163.com>
2024-04-23 13:59:46 +0800

    21444636

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { env } from 'process'
 import { readFileSync } from 'fs'
 import { Octokit } from 'octokit'
-import { getStringAfter } from './utils.js'
 import { failedWithLogs } from './logs.js'
 
 const catchEnv = (name: string) => {
@@ -32,33 +31,4 @@ const prepareRequest = () => {
 
 const octokit = new Octokit(prepareRequest())
 const { data: user } = await octokit.rest.users.getAuthenticated()
-
-const createNewRelease = async () => {
-  let data = {
-    targetCommitish: '',
-    repositoryName: ''
-  }
-  /** GITHUB_REF: 'refs/heads/master' */
-  catchEnv('GITHUB_REF')
-    .then((ref) => (data.targetCommitish = getStringAfter(ref, '/', 2)))
-    .catch((err) => failedWithLogs(err))
-  /** GITHUB_REF: 'username/repositoryname' */
-  catchEnv('GITHUB_REPOSITORY')
-    .then((repo) => (data.repositoryName = getStringAfter(repo, '/')))
-    .catch((err) => failedWithLogs(err))
-  await octokit.request('POST /repos/{owner}/{repo}/releases', {
-    owner: user.login,
-    repo: data.repositoryName,
-    tag_name: vVersion,
-    target_commitish: data.targetCommitish,
-    name: vVersion,
-    body: 'Description of the release',
-    draft: false,
-    prerelease: false,
-    generate_release_notes: false,
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
-  })
-}
-createNewRelease()
+console.log(user.login)
